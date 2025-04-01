9bdbf210dac1b9ca9980c663ed9b1c11bb0ec572
yasmina <y32781093@gmail.com>
2025-04-01 16:45:38 +0200

    creation du router

---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * le router est chargé d'écouter les
+ *  requetes http provenant du client web 
+ * puis d'y repondre avec le contenu approprié
+ */
+
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo "page d'acceuil";
+        break;
+    case '/about':
+        echo "page de presentation";
+        break;
+    case '/skills':
+        echo "page de compétence";
+        break;
+    case '/projects':
+        echo "page de projets";
+        break;
+    case '/contact':
+        echo "page de contact";
+        break;
+    default:
+        echo "page 404, introuvable";
+        break;
+}
