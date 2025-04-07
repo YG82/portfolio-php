d44812fdf9ca22e640aae696124149e558dc8632
yasmina <y32781093@gmail.com>
2025-04-07 16:43:36 +0200

    changements

--- templates/components/footer.php
+++ templates/components/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-indigo-700 text-white fixed bottom-0 w-full">
+<footer class="bg-slate-800 mt-10 text-white w-full">
     <div class="p-2 text-center">
         <p>© <?php echo date('Y'); ?> Ton prénom</p>
     </div>
--- templates/contact.php
+++ templates/contact.php
@@ -1,26 +1,22 @@
+<?php 
+/** 
+ * Page contact
+ * Traitement du formulaire de contact 
+ * et envoi de l'email avec MailTrap
+ */
+
+$firstname = htmlspecialchars(
+    $_POST['firstname'] ?? '',
+    ENT_HTML5,
+    // "UTF-8"
+);
+$lastname = htmlspecialchars($_POST['lastname'] ?? '');
+?>
+
 <h1 class="py-10 bg-clip-text bg-gradient-to-br from-slate-800 to-slate-400 text-6xl font-bold text-transparent text-center">
     Me contacter
 </h1>
 
-<!-- Nom(r) / Prenom(r)
-Mail(r) / Telephone
-Objet (Autre, formation, collaboration, Devis,)
-Message -->
-<div class="m-4 shadow-sm">
-    <form action="" method="post" class="">
-        <div class="flex gap-4 my-4">
-            <div class="flex flex-col gap-2">
-                <label for="firstname" class="font-semibold text-lg">Votre prénom</label>
-                <input type="text" name="firstname" id="firstname" placeholder="Martin" value="Martine" minlength="2" maxlength="80" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" required>
-            </div>
-            <div class="flex flex-col gap-2">
-                <label for="lastname" class="font-semibold text-lg">Votre nom</label>
-                <input type="text" name="lastname" id="lastname" placeholder="Dupont" value="Dupont" minlength="2" maxlength="80" class="border border-slate-200 rounded-lg px-4 py-2 focus:border-slate-400 focus:shadow-md focus:outline-none" required>
-            </div>
-        </div>
-
-        <button type="submit" class="bg-white text-slate-800 rounded-lg shadow-md p-2 mt-4 hover:text-white hover:bg-slate-800">
-            Envoyer
-        </button>
-    </form>
+<div class="grid place-items-center">
+    <?php include 'templates/components/form.php'; ?>
 </div>
