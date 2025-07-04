13753ea7ec47db5c3432aa3cbe7578c8665dae63
yasmina <y32781093@gmail.com>
2025-07-04 10:31:20 +0200

    update footer to reflect correct name in copyright notice

--- templates/components/footer.php
+++ templates/components/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-slate-800 fixed bottom-0  text-white w-full">
     <div class="p-2 text-center">
-        <p>© <?php echo date('Y'); ?> Ton prénom</p>
+        <p>© <?php echo date('Y'); ?> Yasmina</p>
     </div>
 </footer>
 
